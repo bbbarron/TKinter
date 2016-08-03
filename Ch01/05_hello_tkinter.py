88f8c3f3b02a8d1ba62086f7e9c39def45f2091a
bbbarron <barrybbarron@gmail.com>
2016-08-03 10:21:10 -0700

    Adding all exercise files for Lynda tutorial

---
+++ Ch01/05_hello_tkinter.py
@@ -0,0 +1,11 @@
+###!/Users/bbarron/anaconda/envs/py35/bin
+# hello_tkinter.py by Barron Stone
+# This is an exercise file from Python GUI Development with Tkinter on lynda.com
+import sys
+from Tkinter import *
+print (sys.version)
+print (sys.executable)
+#root = Tk()
+#Label(root, text="Hello, Tkinter!").pack()
+
+#root.mainloop()
